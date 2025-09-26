851b2cd235c6a660392a385ea7fc739644ba773a
Induwara <induwaragunasena.sl@gmail.com>
2025-09-26 21:49:58 +0530

    improved

--- snn-fpga/snn-fpga.srcs/sources_1/new/display_digit.vhd
+++ snn-fpga/snn-fpga.srcs/sources_1/new/display_digit.vhd
@@ -1,39 +1,36 @@
+-- display_digit.vhd
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity display_digit is
   port(
-    clk       : in  std_logic;
-    spikes_out: in  std_logic_vector(9 downto 0);  -- SNN output
-    an        : out std_logic_vector(3 downto 0);  -- display anodes
-    seg       : out std_logic_vector(6 downto 0)   -- 7-seg segments
+    clk   : in  std_logic;
+    digit : in  std_logic_vector(3 downto 0);  -- 0..9
+    an    : out std_logic_vector(3 downto 0);
+    seg   : out std_logic_vector(6 downto 0)
   );
 end entity;
 
 architecture rtl of display_digit is
-  signal digit_bin : std_logic_vector(3 downto 0);
 begin
+  -- single-digit static display (AN0 active low)
+  an <= "1110";
 
-  -- Convert spikes_out (one-hot) to binary digit
-  process(spikes_out)
-    variable i : integer;
+  process(digit)
   begin
-    digit_bin <= (others => '0');  -- default
-    for i in 0 to 9 loop
-      if spikes_out(i) = '1' then
-        digit_bin <= std_logic_vector(to_unsigned(i, 4));
-      end if;
-    end loop;
+    case digit is
+      when "0000" => seg <= "0000001"; -- 0
+      when "0001" => seg <= "1001111"; -- 1
+      when "0010" => seg <= "0010010"; -- 2
+      when "0011" => seg <= "0000110"; -- 3
+      when "0100" => seg <= "1001100"; -- 4
+      when "0101" => seg <= "0100100"; -- 5
+      when "0110" => seg <= "0100000"; -- 6
+      when "0111" => seg <= "0001111"; -- 7
+      when "1000" => seg <= "0000000"; -- 8
+      when "1001" => seg <= "0000100"; -- 9
+      when others => seg <= "1111111"; -- blank
+    end case;
   end process;
-
-  -- Connect to 7-segment decoder
-  seg_dec: entity work.seven_seg_decoder
-    port map(
-      digit => digit_bin,
-      seg   => seg
-    );
-
-  -- Activate only AN0
-  an <= "1110";  -- AN0 active low
 end architecture;
